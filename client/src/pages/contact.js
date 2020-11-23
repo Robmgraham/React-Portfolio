10052c720ef25dba877d516edf5e4bdd1920fb6f
rancid <robmgraham13@yahoo.com>
2020-11-22 16:04:46 -0800

    contact page errors were cleaned by adding closing tags

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -3,15 +3,15 @@ import Nav  from "../components"
 import Bootstrap from "bootstrap"
 
 function Contact () {
-    return ({
+    return (
         <>
         <NavBar/>
-        <div class="container"/>
-        <div class="row"/>
-            <div class="col-md-5"/>
-                <form/>
+        <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <form>
                     {/* <!-- name form --> */}
-                    <div class="form-group"/>
+                    <div class="form-group">
                         <label class="speech-bubble" for="exampleInputEmail1">Name</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name"/>
 
@@ -28,8 +28,11 @@ function Contact () {
                         <label class="speech-bubble" for="exampleFormControlTextarea1">Message</label>
                         <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Type Message here" rows="8">  </textarea>
                     </div>
-                <form>
-                   
+            </div>
+            </form>
+            </div>
+             </div> 
+             </div> 
         </>
     )
 }
