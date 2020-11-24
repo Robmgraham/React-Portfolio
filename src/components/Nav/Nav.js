f99ef57f26660d960463976df929ac886a320dcf
rancid <robmgraham13@yahoo.com>
2020-11-24 09:55:42 -0800

    tried replacing public files to original files to fix issue with manifest

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav'
 function NavBar (){
     return (
         <Nav class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Robert Graham</a>
+        <a class="navbar-brand" href="">Robert Graham</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
