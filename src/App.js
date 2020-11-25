795e1d71c60acca8a300e2aadeda9d89929dfc44
rancid <robmgraham13@yahoo.com>
2020-11-24 20:45:32 -0800

    adsf

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Home from "./pages/about.js"
 function App() {
   return (
     <Router>
-      <Route exact path={process.env.PUBLIC_URL} component={Home}/>
-
+      {/* <Route exact path={process.env.PUBLIC_URL} component={Home}/> */}
+    <Home/>
     </Router>
   );
 }
