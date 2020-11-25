3131e4bab55f662b02ce78865b4f8b8642350b4a
rancid <robmgraham13@yahoo.com>
2020-11-25 15:27:47 -0800

    changed <a> on Nav bar to Link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Nav from 'react-bootstrap/Nav'
 import "./style.css"
+import {Link} from "react-router-dom"
 
 function NavBar (){
     return (
         <Nav class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="">Robert Graham</a>
+        <Link class="navbar-brand" href="">Robert Graham</Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/">About</a>
+                    <Link class="nav-link" href="/">About</Link>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/portfolio">Projects/Apps</a>
+                    <Link class="nav-link" href="/portfolio">Projects/Apps</Link>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact<span class="sr-only">(current)</span></a>
+                    <Link class="nav-link" href="/contact">Contact<span class="sr-only">(current)</span></Link>
                 </li>
             </ul>
         </div>
