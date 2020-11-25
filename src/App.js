c3dcc7479f9165b6ef3fd0f33e534d71919051fa
rancid <robmgraham13@yahoo.com>
2020-11-25 15:03:49 -0800

    added react to imports on app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from "./pages/about.js"
 import Contact from "./pages/contact"
