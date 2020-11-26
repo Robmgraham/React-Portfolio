11ce4668d6983e94004c8d4bbb7f343ff8074a65
rancid <robmgraham13@yahoo.com>
2020-11-25 20:19:37 -0800

    removed /nav from import of bootstrap react

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Nav, Button} from 'react-bootstrap/Nav'
+import {Nav, Button} from 'react-bootstrap'
 import "./style.css"
 import {Link} from "react-router-dom"
 
