5b55e1e4e6b978aaf55183fb2b8c73cb8014dd82
rancid <robmgraham13@yahoo.com>
2020-11-25 21:08:54 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react"
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from "./pages/about.js"
 import Contact from "./pages/contact.js"
 import Portfolio from "./pages/portfolio.js"
 function App() {
   return (
-    <Router>
+    <Home/>
+    // <Router>
       
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/contact" exact component={Contact}/>
-        <Route path="/portfolio" exact component={Portfolio}/>
-      </Switch>
+    //   <Switch>
+    //     <Route path="/" exact component={Home}/>
+    //     <Route path="/contact" exact component={Contact}/>
+    //     <Route path="/portfolio" exact component={Portfolio}/>
+    //   </Switch>
       
-    </Router>
+    // </Router>
   );
 }
 
