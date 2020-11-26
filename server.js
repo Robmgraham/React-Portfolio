f74e98fcb623a03eeef1a9e4889e28faa828727c
rancid <robmgraham13@yahoo.com>
2020-11-25 21:32:34 -0800

    adf

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3001
+const path = require("path")
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, "../build/index.html"));
+    });
+
+
+}
+
+
 app.listen(PORT, function(){
     console.log("app is listening on port " + PORT)
 })
